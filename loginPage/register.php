06d43b4f7c5a17e26c5bb9ba387965f34080cc7d
ardiona <ardionarahadi@gmail.com>
2024-08-27 16:26:44 +0700

    Menambahkan Sistem Registrasi

--- loginPage/register.php
+++ loginPage/register.php
@@ -1,7 +1,5 @@
 <?php
 include "../config/controller.php";
-$eror = "";
-$erorPasswd = "";
 if (isset($_POST["register"])) {
   $email = $_POST["email"];
   $username = $_POST["username"];
@@ -10,10 +8,25 @@ if (isset($_POST["register"])) {
 
   if($username == "" || $email == "" || $password == "" || $confirmPassword == ""){
     $eror = true;
-  } 
-  if ($confirmPassword !== $password){
-    $erorPasswd = true;
+  }  else if ($confirmPassword !== $password){
+    $erorPw= true;
+  } else {
+    
+    $hasedPw = password_hash($password, PASSWORD_DEFAULT);
+    $select_email = mysqli_query($db_conn, "SELECT * FROM t_users WHERE email = '$email'");
+    $select_username = mysqli_query($db_conn, "SELECT * FROM t_users WHERE username = '$username'");
+
+    if (mysqli_num_rows($select_email) > 0){
+        $erorEmail = true;
+    }
+    if (mysqli_num_rows($select_username) > 0){
+        $erorUsername = true;
+    } else {
+        $success = true;
+        mysqli_query($db_conn, "INSERT INTO t_users VALUES(NULL, '$email', '$username', '$hasedPw')");
+    }
   }
+
 }
 ?>
 
@@ -41,14 +54,25 @@ if (isset($_POST["register"])) {
         <form method="post" accept-charset="utf-8">
             <h1>Sign up</h1>
 
-            <?php if($eror) : ?>
+            <?php if(isset($eror)) : ?>
             <span>Semua Inputan Wajib Di Isi</span>
             <?php endif ?>
 
+            <?php if(isset($erorEmail)) : ?>
+            <span>Email Sudah Terdaftar</span>
+            <?php endif ?>
+
+            <?php if(isset($success)) : ?>
+            <span class="success">Registrasi Berhasil Silahkan Login</span>
+            <?php endif ?>
             <div class="form_content">
                 <label for="email"><i class="bx bx-envelope"></i></label>
                 <input class="email username" type="email" name="email" id="email" placeholder="Email" />
             </div>
+
+            <?php if(isset($erorUsername)) : ?>
+            <span>Username Sudah Digunakan, Gunakan Username Lain</span>
+            <?php endif ?>
             <div class="form_content">
                 <label for="username"><i class="bx bx-user"></i></label>
                 <input class="username" type="text" name="username" id="username" placeholder="Username" />
@@ -57,21 +81,23 @@ if (isset($_POST["register"])) {
                 <label for="password"><i class="bx bx-lock"></i></label>
                 <input class="password" type="password" name="password" id="password" placeholder="Password" />
             </div>
+
+            <?php if(isset($erorPw)) : ?>
+            <span>Password Tidak Sama</span>
+            <?php endif ?>
             <div class="form_content">
                 <label for="password"><i class="bx bx-lock"></i></label>
                 <input class="password" type="password" name="confirmPassword" id="confirmPasswordPpassword"
                     placeholder="Confirm Password" />
             </div>
-            <?php if($erorPasswd) : ?>
-            <span>Password Tidak Sama</span>
-            <?php endif ?>
+
             <div class="checkbox showpw">
                 <input onclick="showPasswd()" id="showPw" type="checkbox" />
                 <label for="showPw">Show Password</label>
             </div>
             <div class="checkbox">
-                <input onclick="showPasswd()" id="showPw" type="checkbox" />
-                <label for="showPw">I Accept <a href="#">Terms and Services</a></label>
+                <input id="checkbox" type="checkbox" required />
+                <label for="checkbox">I Accept <a href="#">Terms and Services</a></label>
             </div>
 
             <button type="submit" name="register">Sign up</button>
